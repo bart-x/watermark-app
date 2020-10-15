f43ae8d531a9ea74d82c9fb6dff6555234ecf46d
Bartek K <kontener00@gmail.com>
2020-10-16 00:44:08 +0200

    add watermark logic instructions

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
 const Jimp = require('jimp');
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 const addTextWatermarkToImage = async function (inputFile, outputFile, text) {
-    const image = await Jimp.read(inputFile);
-    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-    const textData = {
-        text: text,
-        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-    };
+    try {
+        const image = await Jimp.read(inputFile);
+        const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+        const textData = {
+            text,
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+        };
 
-    image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
-    await image.quality(100).writeAsync(outputFile);
+        image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+        await image.quality(100).writeAsync(outputFile);
+    } catch (error) {
+        console.log('something went wrong :(. Try again');
+    }
+    console.log('File with watermark upload succesfully');
+    startApp();
 };
 
 const addImageWatermarkToImage = async function (inputFile, outputFile, watermarkFile) {
-    const image = await Jimp.read(inputFile);
-    const watermark = await Jimp.read(watermarkFile);
-    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
-    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+    try {
+        const image = await Jimp.read(inputFile);
+        const watermark = await Jimp.read(watermarkFile);
 
-    image.composite(watermark, x, y, {
-        mode: Jimp.BLEND_SOURCE_OVER,
-        opacitySource: 0.5,
-    });
-    await image.quality(100).writeAsync(outputFile);
+        const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+        const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+        image.composite(watermark, x, y, {
+            mode: Jimp.BLEND_SOURCE_OVER,
+            opacitySource: 0.5,
+        });
+        await image.quality(100).writeAsync(outputFile);
+    } catch (error) {
+        console.log('something went wrong :(. Try again');
+    }
+    console.log('File with watermark upload succesfully');
+    startApp();
 };
+
 
 const prepareOutputFilename = (filename) => {
     const [name, ext] = filename.split('.');
@@ -61,9 +76,13 @@ const startApp = async () => {
             name: 'value',
             type: 'input',
             message: 'Type your watermark text:',
-        }])
+        }]);
         options.watermarkText = text.value;
-        addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
+        if (fs.existsSync('./img/' + options.inputImage)) {
+            addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
+        } else {
+            console.log('file not found, check if you spell file name correctly');
+        }
     }
     else {
         const image = await inquirer.prompt([{
@@ -71,11 +90,14 @@ const startApp = async () => {
             type: 'input',
             message: 'Type your watermark name:',
             default: 'logo.png',
-        }])
+        }]);
         options.watermarkImage = image.filename;
-        addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), './img/' + options.watermarkImage);
+        if (fs.existsSync('./img/' + options.inputImage) && fs.existsSync('./img/' + options.watermarkImage)) {
+            addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), './img/' + options.watermarkImage);
+        } else {
+            console.log('file not found, check if you spell file name correctly');
+        }
     }
-
-};
+}
 
 startApp();
